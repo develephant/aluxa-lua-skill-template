43fea28d68cc4a681d9c18df66e81db531bd0820
develephant <code@develephant.com>
2019-11-03 14:04:42 -0600

    1.0

--- hooks/post_new_hook.sh
+++ hooks/post_new_hook.sh
@@ -11,7 +11,7 @@
 
 SKILL_NAME=$1
 
-sed -i .bak "s/ask-custom-${SKILL_NAME}-default/https:\/\/aluxa.server.domain\/$SKILL_NAME/g" $SKILL_NAME/skill.json
+sed -i .bak "s/ask-custom-${SKILL_NAME}-default/https:\/\/aluxa.domain.tld\/$SKILL_NAME/g" $SKILL_NAME/skill.json
 
 mv $SKILL_NAME/lua/SkillName $SKILL_NAME/lua/$SKILL_NAME
 
