0e6dcbab47ba9de56aa4229bf218295f6a8edda2
develephant <code@develephant.com>
2019-11-03 13:59:47 -0600

    1.0

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Starter Aluxa skill template for creating Amazon Alexa Skills using the ASK CLI 
 
 ## new
 
-`ask new --url https://github.com/develephant/aluxa-lua-skill-template.git -n <SKILL>`
+`ask new --url https://github.com/develephant/aluxa-lua-skill-template.git -n <SKILL_NAME>`
 
 ## deploy
 
@@ -12,4 +12,18 @@ Starter Aluxa skill template for creating Amazon Alexa Skills using the ASK CLI 
 
 ## simulate
 
-`ask simulate -l en-US -t "start <SKILL>"`
+`ask simulate -l en-US -t "start <SKILL_NAME>"`
+
+## Using `ask api`
+
+### create-skill
+
+`ask api create-skill -f skill.json`
+
+### update-model
+
+`ask api update-model -s <SKILL_ID> -l en-US -f models/en-US.json`
+
+### delete-skill
+
+`ask api delete-skill -s <SKILL_ID>`
--- hooks/post_new_hook.ps1
+++ hooks/post_new_hook.ps1
@@ -1,53 +1,22 @@
-# Powershell script for ask-cli post-new hook for Node.js
-# Script Usage: post_new_hook.ps1 <SKILL_NAME> <DO_DEBUG> <TARGET>
+# Powershell script for ask-cli post-new hook for Aluxa.
+# Script Usage: post_new_hook.ps1 <SKILL_NAME>
  
 # SKILL_NAME is the preformatted name passed from the CLI, after removing special characters.
-# DO_DEBUG is boolean value for debug logging
  
 # Run this script one level outside of the skill root folder
  
 # The script does the following:
-#  - Run "npm install" in each sourceDir in skill.json
+#  - Renames and cleans up template items.
 
-# param( 
-#     [string] $SKILL_NAME,
-#     [bool] $DO_DEBUG = $False
-# )
+param( 
+    [string] $SKILL_NAME
+)
 
-# if ($DO_DEBUG) {
-#     Write-Output "###########################"
-#     Write-Output "###### post-new hook ######"
-#     Write-Output "###########################"
-# }
+$SKILL_FILE_PATH = $SKILL_NAME + "\skill.json"
 
-# function install_dependencies ($CWD, $SOURCE_DIR) {
-#     $INSTALL_PATH = $SKILL_NAME + "\" +$SOURCE_DIR
-#     Set-Location $INSTALL_PATH
-#     Invoke-Expression "npm install" 2>&1 | Out-Null
-#     $EXEC_RESULT = $?
-#     Set-Location $CWD
-#     return $EXEC_RESULT
-# }
+Rename-Item $SKILL_NAME + "\lua\SkillName" $SKILL_NAME + "\lua\" + $SKILL_NAME
 
-# $SKILL_FILE_PATH = $SKILL_NAME + "\skill.json"
-# $ALL_SOURCE_DIRS = Get-Content -Path $SKILL_FILE_PATH | select-string  -Pattern "sourceDir" -CaseSensitive
-# Foreach ($SOURCE_DIR in $ALL_SOURCE_DIRS) {
-#     $FILTER_SOURCE_DIR = $SOURCE_DIR -replace "`"", "" -replace "\s", "" -replace ",","" -replace "sourceDir:", ""
-#     $CWD = (Get-Location).Path
-#     if (install_dependencies $CWD $FILTER_SOURCE_DIR) {
-#         if ($DO_DEBUG) {
-#             Write-Output "Codebase ($FILTER_SOURCE_DIR) built successfully."
-#         }
-#     } else {
-#         if ($DO_DEBUG) {
-#             Write-Output "There was a problem installing dependencies for ($FILTER_SOURCE_DIR)."
-#         }
-#         exit 1
-#     }
-# }
-
-# if ($DO_DEBUG) {
-#     Write-Output "###########################"
-# }
+Remove-Item $SKILL_NAME + "\skill.json.bak"
+Remove-Item $SKILL_NAME + "\.git" -Recurse -Force
 
 exit 0
